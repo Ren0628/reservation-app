0291ef63f4c0b185d26bba0ef634d09382bf68ff
Ren0628 <ren6nt@gmail.com>
2024-03-14 16:54:00 +0900

    データベーステーブル作成とリレーション設定

---
+++ app/Models/Reservation.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Reservation extends Model
+{
+    use HasFactory;
+
+    public function ReservationDetails()
+    {
+        return $this->hasMany(ReservationDetail::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
---
+++ app/Models/ReservationDetail.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ReservationDetail extends Model
+{
+    use HasFactory;
+
+    public function room()
+    {
+        return $this->belongsTo(Room::class);
+    }
+
+    public function reservation()
+    {
+        return $this->belongsTo(Reservation::class);
+    }
+}
